ca97d151c024069628202f0e9c9c0bca66d3a930
RomanGolovinn <golovinroman.ev@gmail.com>
2026-07-18 15:37:35 +0300

    fix: updated db models for compatibility with postgres schema

--- internal/storage/postgres/models.go
+++ internal/storage/postgres/models.go
@@ -2,6 +2,25 @@ package postgres
 
 import "time"
 
+type PermissionType string
+
+const (
+	None            PermissionType = "none"
+	PaymentInitiate PermissionType = "payment:initiate"
+	PaymentApprove  PermissionType = "payment:approve"
+	PaymentExecute  PermissionType = "payment:execute"
+	PaymentReverse  PermissionType = "payment:reverse"
+
+	AccountReadBalance PermissionType = "account:read:balance"
+	AccountReadHistory PermissionType = "account:read:history"
+	AccountReadPii     PermissionType = "account:read:pii"
+
+	ConfigLimitsWrite PermissionType = "config:limits:write"
+
+	ProductManage  PermissionType = "product:manage"
+	SystemOverride PermissionType = "system:override"
+)
+
 type Client struct {
 	Id      string
 	Name    string
@@ -16,7 +35,29 @@ type Server struct {
 }
 
 type Permission struct {
+	Id         int
 	ClientId   string
 	ServerId   string
-	Permission int
+	Permission PermissionType
+}
+
+type IssuedCredential struct {
+	Id               string
+	ClientId         string
+	ServerId         string
+	TempUsername     string
+	TempPasswordHash string
+	CreatedAt        time.Time
+	ValidTo          time.Time
+	RevokedAt        time.Time
+}
+
+type AuditLog struct {
+	Id        string
+	Timestamp time.Time
+	ClientId  string
+	ServerId  string
+	EventType string
+	IpAddress string
+	Details   string // JSONB stored as string
 }
