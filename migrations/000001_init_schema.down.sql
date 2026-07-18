29db5ed3f2bb8509654df417c53e4f7443718ad2
RomanGolovinn <golovinroman.ev@gmail.com>
2026-07-18 15:16:38 +0300

    feat: expanded the schema, added a table for logs and for storing all allocated data, created an enum for permissions, and created an index for rapid lookup of allocated data based on validity

--- migrations/000001_init_schema.down.sql
+++ migrations/000001_init_schema.down.sql
@@ -1,3 +1,7 @@
 drop table if exists permissions;
 drop table if exists client;
 drop table if exists server;
+drop index if exists idx_issued_credentials_valid_to;
+drop table if exists issued_credentials;
+drop type if exists permission_type;
+drop table if exists audit_log;
